d5bab849a9e1d782a56c744a2d34c8b2191b5baa
Gonzalo <gonzalomalaspina2005@gmail.com>
2025-05-28 14:13:38 +0000

    faltan los comentarios, pero funciona

---
+++ ej3/example/main.cpp
@@ -0,0 +1,33 @@
+
+#include "../headers/vectortostring.hpp"
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+    keyValue json;
+    Vecreator elementos;
+
+    vector<int> vec1 = {1,2};
+    vector<int> vec2 = {3,4};
+    string s1 = "Hola";
+    string s2 = "Mundo";
+
+    elementos.agregar(1.3);
+    elementos.agregar(2.1);
+    elementos.agregar(3.2);
+    elementos.agregar(s1);
+    elementos.agregar(s2);
+    elementos.agregar(vec1);
+    elementos.agregar(vec2);
+
+    json.mapeo("listas", elementos.getVecMatriz());
+    json.mapeo("palabras", elementos.getVecString());
+    json.mapeo("vec_doubles", elementos.getVecDouble());
+    cout << json.imprimirJson() << endl;
+    return 0;
+}
